15ee652cd147da8cd010c7c8fb52e6cd108abb89
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-11-25 10:16:43 +0100

    Pagina registrazioine

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script>
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
+import AppRegistration from "./components/AppRegistration.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Header
-  }
-}
+	name: "App",
+	components: {
+		Header,
+		AppRegistration,
+	},
+};
 </script>
 <template>
-  <Header />
+	<Header />
+	<AppRegistration />
 </template>
-
